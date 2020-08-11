8f39401cb7beea360f3aa020e9297986dc7767e7
Herick Exterkoetter <herickherick47@gmail.com>
2020-08-11 20:43:49 -0300

    usando uma funçao no signIn pelo context

--- src/context/AuthContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-// usar informações em varios lugares
-interface AuthContextData {
-  name: string;
-}
-
-const AuthContext = createContext<AuthContextData>({} as AuthContextData);
-
-export default AuthContext;
