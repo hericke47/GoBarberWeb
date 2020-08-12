f1ea9aef50795c427b0115c2931f7aac750da590
Herick Exterkoetter <herickherick47@gmail.com>
2020-08-12 03:35:30 -0300

    fazendo o toast ser removido depois de alguns segundos

--- src/components/ToastContainer/Toast/index.tsx
+++ src/components/ToastContainer/Toast/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
 
 import { ToastMessage, useToast } from '../../../hooks/toast';
@@ -11,6 +11,16 @@ interface ToastProps {
 
 const Toast: React.FC<ToastProps> = ({ message }) => {
   const { removeToast } = useToast();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      removeToast(message.id);
+    }, 4000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [removeToast, message.id]);
 
   return (
     <Container type={message.type} hasDescription={!!message.description}>
